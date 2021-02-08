c7320f6a4ef13c6c4fac5961f863130e9ee3c02f
paul <dev@positivemint.com>
2021-02-08 13:54:41 +0000

    updating

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -11,6 +11,7 @@ class AppKernel extends Kernel
         $bundles = [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
